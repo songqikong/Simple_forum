78873125e1a9e0123228b057207604814d10d6b0
kkans <kkanshan@outlook.com>
2020-07-03 17:29:18 +0800

    the empty project file import

---
+++ src/main/java/com/kkanshan/webfourm/WebFourmApplication.java
@@ -0,0 +1,13 @@
+package com.kkanshan.webfourm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFourmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFourmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkanshan/webfourm/WebFourmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkanshan.webfourm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFourmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
