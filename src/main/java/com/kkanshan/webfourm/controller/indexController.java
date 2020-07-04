3b240ba442d1354167643c57836c705aab8e0d15
kkans <kkanshan@outlook.com>
2020-07-04 20:41:40 +0800

    导航条测试成功

--- src/main/java/com/kkanshan/webfourm/controller/indexController.java
+++ src/main/java/com/kkanshan/webfourm/controller/indexController.java
@@ -1,14 +1,13 @@
 package com.kkanshan.webfourm.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
 
-    @GetMapping("/index");
-    public String index{
+    @GetMapping("/index")
+    public String index(){
         return "index";
     }
 }
